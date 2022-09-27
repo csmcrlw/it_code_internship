165cd71f92ee409d3ced30e4ea8de525e28c2a85
csmcrlw <lidaaaserova@gmail.com>
2022-09-28 01:24:37 +0500

    #1 init

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 import core.views
 
 urlpatterns = [
-    path('', core.views.index),
-
+    path('', core.views.Index.as_view()),
+    path('persons/', core.views.Person.as_view()),
+    path('persons/<int:id>/', core.views.person),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from core import models
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
-def index(request):
-    person = models.Person.objects.first()
-    response = render(request, 'core/index.html', context={'person': person})
-    return response
+
+class Index(TemplateView):
+    template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        c = super().get_context_data(**kwargs)
+        c['person_name'] = 'Masha'
+        return c
+
+class Person(ListView):
+    model = models.Person
+
+    # def get(self, request, *args, **kwargs):
+    #     response = super().get(request, *args, **kwargs)
+    #     return response
+
+
+# def persons(request):
+#     '''Данные всех заказчиков'''
+#     object_list = []
+#     for p in models.Person.objects.all():
+#         object_list.append({
+#             'id': p.id,
+#             'name': p.name,
+#         })
+#     return JsonResponse({'results': object_list})
+
+def person(request, id):
+    '''Данные заказчика с его id'''
+    if request.method == "GET":
+        p = get_object_or_404(models.Person, id=id)
+        detail = {
+            'id': p.id,
+            'name': p.name,
+            'phone': p.phone,
+        }
+    return JsonResponse(detail)
