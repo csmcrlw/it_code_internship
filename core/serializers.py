60ba6dea9b6b62aa2005438a3847dd9643fd3279
csmcrlw <lidaaaserova@gmail.com>
2022-10-14 22:34:52 +0500

    #5 добавили возможность входа на сайт и регистрации

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,32 @@ from rest_framework import serializers
 from core import models
 
 
+class RegisterUser(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8)
+
+    def validate_username(self, value):
+        if models.User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Пользователь с таким именем уже есть')
+        return value
+
+
+class LoginUser(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8)
+
+    def validate_username(self, value):
+        if not models.User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Пользователь с таким именем не найден')
+        return value
+
+    def validate(self, attrs):
+        user = models.User.objects.get(username=attrs['username'])
+        if not user.check_password(attrs['password']):
+            raise serializers.ValidationError({'password': 'Неверный пароль'})
+        return attrs
+
+
 class Tag(serializers.ModelSerializer):
     display = serializers.SerializerMethodField()
     class Meta:
@@ -40,30 +66,7 @@ class Item(serializers.ModelSerializer):
         exclude = ('user', )
 
 
-class RegisterUser(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(min_length=8)
 
-    def validate_username(self, value):
-        if models.User.objects.filter(username=value).exists():
-            raise serializers.ValidationError('Пользователь с таким именем уже есть')
-        return value
-
-
-class LoginUser(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(min_length=8)
-
-    def validate_username(self, value):
-        if not models.User.objects.filter(username=value).exists():
-            raise serializers.ValidationError('Пользователь с таким именем не найден')
-        return value
-
-    def validate(self, attrs):
-        user = models.User.objects.get(username=attrs['username'])
-        if not user.check_password(attrs['password']):
-            raise serializers.ValidationError({'password': 'Неверный пароль'})
-        return attrs
 
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
 import core.views
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+urlpatterns = [
+    path('user/register/', core.views.RegisterUser.as_view()),
+    path('user/login/', core.views.LoginUser.as_view()),
+]
 
 router = DefaultRouter()
 router.register('tags', core.views.TagViewSet, basename='tag')
